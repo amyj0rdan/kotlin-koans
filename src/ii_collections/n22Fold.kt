b84bf795177e768ef15d8dbdac23b92d5b489a14
Amy Jordan <amy.jordan@johnlewis.co.uk>
2019-07-19 16:43:38 +0100

    Removes explicit return

--- src/ii_collections/n22Fold.kt
+++ src/ii_collections/n22Fold.kt
@@ -12,9 +12,8 @@ fun whatFoldDoes(): Int {
     return result
 }
 
-fun Shop.getSetOfProductsOrderedByEachCustomer(): Set<Product> {
+fun Shop.getSetOfProductsOrderedByEachCustomer(): Set<Product> =
     // Return the set of products that were ordered by each of the customers
-    return customers.fold(allOrderedProducts) { orderedByAll, customer ->
+    customers.fold(allOrderedProducts) { orderedByAll, customer ->
         orderedByAll.intersect(customer.orderedProducts)
     }
-}
