0bf6af4f91f937a9e6ec9cc41ad82d4d4a817e0f
Amy Jordan <amy.jordan@johnlewis.co.uk>
2019-07-19 12:53:36 +0100

    Completes exercises 27 and 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -8,12 +8,25 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparabl
     }
 }
 
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
     operator fun contains(item: MyDate): Boolean = start <= item && item <= endInclusive
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var startDate: MyDate = dateRange.start
+    override fun next(): MyDate {
+        val result = startDate
+        startDate = startDate.nextDay()
+        return result
+    }
+    override fun hasNext(): Boolean = startDate <= dateRange.endInclusive
 }
